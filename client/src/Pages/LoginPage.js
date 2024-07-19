bf80e67bff441b331e9a7055155e001ad5c41563
Riley Cheung <tcheung25@mylangara.ca>
2024-07-18 21:38:10 -0700

    Updated LoginPage.js. Added business login component.

--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -4,9 +4,39 @@ import axios from "axios";
 function LoginPage () {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [role, setRole] = useState("client");
   const [error, setError] = useState("");
 
-  const loginHandler = (e) => {
+  const businessRoleChangeHandler = () => {
+    setRole("business");
+  }
+
+  const clientRoleChangeHandler = () => {
+    setRole("business");
+  }
+
+  const clientLoginHandler = (e) => {
+    e.preventDefault();
+    try {
+      const response = axios.post("", {
+        email: userName,
+        password: password
+      })
+      if (response.data.result === "Login Successful") {
+        localStorage.setItem("ImmivanRole", JSON.stringify(response.data.role));
+        // redirect to dashboard
+        window.location.href = "/dashboard"
+      } else {
+        // console.log("Invalid email or password")
+        setError("Invalid email or password")
+      }
+    } catch (err) {
+      console.error('Error:', err);
+      alert('An error occurred. Please try again.');
+    }
+  }
+
+  const businessLoginHandler = (e) => {
     e.preventDefault();
     try {
       const response = axios.post("", {
@@ -37,23 +67,44 @@ function LoginPage () {
     setPassword(e.target.value);
   }
 
+  const clientLoginUI = (
+    <form id="loginForm" method="POST" action="/login" onSubmit={clientLoginHandler}>
+      <div>
+        <label htmlFor="email">Email Address:</label>
+        <input type="email" id="email" name="email" aria-required="true" onChange={usernameInputHandler}required />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" aria-required="true" onChange={passwordInputHandler} required />
+      </div>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <div>
+        <button type="submit">Login</button>
+      </div>
+    </form>);
+
+  const businessLoginUI = (
+    <form id="loginForm" method="POST" action="/businesslogin" onSubmit={businessLoginHandler}>
+      <div>
+        <label htmlFor="email">Email Address:</label>
+        <input type="email" id="email" name="email" aria-required="true" onChange={usernameInputHandler}required />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" aria-required="true" onChange={passwordInputHandler} required />
+      </div>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <div>
+        <button type="submit">Login</button>
+      </div>
+    </form>);
+
   return (
     <>
       <h1>User Login</h1>
-      <form id="loginForm" method="POST" action="/login">
-        <div>
-          <label htmlFor="email">Email Address:</label>
-          <input type="email" id="email" name="email" aria-required="true" onChange={usernameInputHandler}required />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" aria-required="true" onChange={passwordInputHandler} required />
-        </div>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-        <div>
-          <button type="submit">Login</button>
-        </div>
-      </form>
+      <button onclick={clientRoleChangeHandler}>Client Login</button>
+      <button onChange={businessRoleChangeHandler}>Business Login</button>
+      {role === "client"? clientLoginUI:businessLoginUI}
     </>
   )
 }

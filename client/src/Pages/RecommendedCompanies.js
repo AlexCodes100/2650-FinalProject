64203be0196a45a4e3007c86aff8e25f51a56b19
Riley Cheung <tcheung25@mylangara.ca>
2024-08-01 04:45:31 -0700

    Disable chat with unfollowed business feature.

--- client/src/Pages/RecommendedCompanies.js
+++ client/src/Pages/RecommendedCompanies.js
@@ -15,11 +15,11 @@ function RecommendedCompanies({ companies,onFollow, onChat }) {
             <Card.Title>{company.businessName}</Card.Title>
             <Card.Text>{company.businessType}</Card.Text>
             <Button variant="primary" onClick={() => onFollow(company.id)}>Follow</Button>
-            <Button variant="secondary" className="ml-2" 
+            {/* <Button variant="secondary" className="ml-2" 
             businessid={company.id}
             chatbusinessname={company.businessName}
             onClick={() => onChat({chatId:company.chatId, businessId:company.id, businessName:company.businessName})}
-            >Chat</Button> 
+            >Chat</Button>  */}
           </Card.Body>
         </Card>
       ))
